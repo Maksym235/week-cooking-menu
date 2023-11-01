bdb63822c4dc13a2addfbf85c12df354de50397c
Maksym235 <andriichukwork235@gmail.com>
2023-11-01 22:40:37 +0300

    comit header

--- src/Pages/Ingredients/Ingredients.tsx
+++ src/Pages/Ingredients/Ingredients.tsx
@@ -1,14 +1,13 @@
 import styles from './Ingredients.module.css'
 // import {Header} from "../../Components/Header/Header.tsx";
-// import {IngredientsList} from "../../Components/IngredientsList/IngredientsList.tsx";
-// import {CreateNewBtn} from "../../Components/CreateNewBtn/CreateNewBtn.tsx";
+import {IngredientsList} from "../../Components/IngredientsList/IngredientsList.tsx";
+import {CreateNewBtn} from "../../Components/CreateNewBtn/CreateNewBtn.tsx";
 const Ingredients = () => {
 return (
 <section className={styles.conteiner}>
-    <p>Ingredients page</p>
     {/*<Header title='Ingredients'/>*/}
-    {/*<IngredientsList/>*/}
-    {/*<CreateNewBtn/>*/}
+    <IngredientsList/>
+    <CreateNewBtn/>
 </section>
 )
 }
